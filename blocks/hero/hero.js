2c7e6c0f30f3f1dff5861c1ec3211a0bcdd66c13
anoopr7 <anoopr7@gmail.com>
2025-08-06 16:10:01 +0530

    test first block

---
+++ blocks/hero/hero.js
@@ -0,0 +1,17 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  
+
+  // decorate footer DOM
+  block.textContent = 'hero decorate';
+  const footer = document.createElement('div');
+  //while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  block.append(footer);
+}
