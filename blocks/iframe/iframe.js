a06763d9f057b6e20c30c090320e41646123086b
anoopr7 <anoopr7@gmail.com>
2025-08-06 17:35:30 +0530

    Iframe component

---
+++ blocks/iframe/iframe.js
@@ -0,0 +1,21 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the Iframe
+ * @param {Element} block The Iframe block element
+ */
+export default async function decorate(block) {
+  
+
+  // decorate Iframe  DOM
+  block.textContent = 'Iframe page';
+ // block.setHTMLUnsafe = '<iframe src="https://www.nexaexperience.com"> </iframe>'
+  const iframe = document.createElement("iframe");
+    iframe.src = "https://dev-nexa.marutisuzuki.com/en/iframe-banner";
+    //iframe.width = "100";
+    //iframe.height = height;
+    iframe.style.border = "none";
+    iframe.allowFullscreen = true;
+  block.append(iframe);
+}
